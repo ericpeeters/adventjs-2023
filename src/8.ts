42af0b2e2c2c7a2302b3ed76d2f9a2054277de32
Eric Peeters <ericpeeters@live.nl>
2023-12-21 20:20:20 +0100

    Submitted solution for day 8

---
+++ src/8.ts
@@ -0,0 +1,36 @@
+function organizeGifts(gifts: string): string {
+  const giftTypes = Array.from(gifts.matchAll(/\d{1,10}/g));
+
+  return giftTypes.reduce((acc, giftTypeMatch) => {
+    const [giftTypeAmount] = giftTypeMatch;
+    const numberOfGiftsInType = parseInt(giftTypeAmount, 10);
+    const symbolIndex = giftTypeMatch.index + giftTypeAmount.length;
+    const symbol = gifts[symbolIndex];
+    const amountsFor = {
+      pallet: 50,
+      box: 10,
+    };
+
+    const pallets = Math.floor(numberOfGiftsInType / amountsFor.pallet);
+    const boxes = Math.floor(
+      (numberOfGiftsInType - pallets * amountsFor.pallet) / amountsFor.box
+    );
+    const leftOvers =
+      numberOfGiftsInType -
+      pallets * amountsFor.pallet -
+      boxes * amountsFor.box;
+
+    return (
+      acc +
+      [
+        `[${symbol}]`.repeat(pallets),
+        `{${symbol}}`.repeat(boxes),
+        `(${symbol.repeat(leftOvers)})`,
+      ].join("")
+    );
+  }, "");
+}
+
+const output = organizeGifts("76a11b");
+
+console.log({ output, testCase: output === "[a]{a}{a}(aaaaaa){b}(b)" });
